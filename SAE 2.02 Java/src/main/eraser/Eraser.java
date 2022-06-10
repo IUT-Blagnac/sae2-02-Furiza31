d5f98216bf162f733009f95bfaf1a69dfd5d028e
Hugo Wendjaneh <hugo.wendjaneh@gmail.com>
2022-06-10 10:01:21 +0100

    Création du projet java sur eclipse

---
+++ SAE 2.02 Java/src/main/eraser/Eraser.java
@@ -0,0 +1,5 @@
+package main.eraser;
+
+public class Eraser {
+
+}
--- src/main/java/eraser/Eraser.java
+++
@@ -1,7 +0,0 @@
-package eraser;
-
-public class Eraser {
-    public static String erase(String str) {
-        throw new UnsupportedOperationException("Unimplemented");
-    }
-}
