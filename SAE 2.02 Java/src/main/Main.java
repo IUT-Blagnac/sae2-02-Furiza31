f0a4147c75047f38b6e0bccf0453bcdca195a5ed
Hugo Wendjaneh <hugo.wendjaneh@gmail.com>
2022-06-10 13:55:59 +0100

    Algo 2 java

--- SAE 2.02 Java/src/main/Main.java
+++ SAE 2.02 Java/src/main/Main.java
@@ -5,8 +5,10 @@ import main.eraser.Eraser;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(Eraser.methodeUn("C o u cou  J M  B "));
-		System.out.println(Eraser.methodeDeux("Cou cou  J M  B"));
+		String test = "C o u cou  J M  B ";
+		System.out.println(Eraser.methodeUn(test));
+		System.out.println(Eraser.methodeDeux(test));
+		System.out.println(Eraser.methodeTrois(test));
 	}
 
 }
--- SAE 2.02 Java/src/main/eraser/Eraser.java
+++ SAE 2.02 Java/src/main/eraser/Eraser.java
@@ -1,30 +1,53 @@
 package main.eraser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Eraser {
+	
+	/**
+	 * Methode avec regex
+	 * @param req
+	 * @return
+	 */
 	public static String methodeUn(String req) {
 		// remplace tous les espaces qui n'ont pas d'espace avant et après par du vide
 		return req.replaceAll("(?<! ) (?! )", ""); 
 	}
 
+	/**
+	 * Methode par création d'une chaine de caractère
+	 * @param req
+	 * @return
+	 */
 	public static String methodeDeux(String req) {
-		String res;
-		int i;
-		char space = ' ';
+		String res = "";
+		int length = req.length();
 		
-		res = "";
-		for (i = 1; i < req.length() - 1; i++) {
-			if (
-					Character.compare(req.charAt(i), space) == 0 &&
-					(
-						Character.compare(req.charAt(i-1), space) != 0 &&
-						Character.compare(req.charAt(i+1), space) != 0
-					)
-				) {
+		if (Character.compare(req.charAt(0), ' ') == 0 && Character.compare(req.charAt(1), ' ') != 0) {
+			res += "";
+		} else res += req.charAt(0);
+		for (int i = 1; i < length - 1; i++) {
+			if (Character.compare(req.charAt(i), ' ') == 0 && Character.compare(req.charAt(i+1), ' ') != 0 && Character.compare(req.charAt(i-1), ' ') != 0) {
 				res += "";
 			} else {
 				res += req.charAt(i);
 			}
 		}
-		return res = "";
+		if (Character.compare(req.charAt(length - 1), ' ') == 0 && Character.compare(req.charAt(length - 2), ' ') != 0) {
+			res += "";
+		} else res += req.charAt(length-2);
+		return res;
 	}
+	
+	/**
+	 * Methode par copy de chaine de caractère pour supprétion
+	 * @param req
+	 * @return
+	 */
+	public static String methodeTrois(String req) {
+		ArrayList<String> res = new ArrayList<String>(Arrays.asList(req.split("")));
+		return res.toString();
+	}
+	
 }
