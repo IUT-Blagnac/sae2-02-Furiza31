95d1d51bb4da0d8c271129cc7a563041a5984eed
Hugo Wendjaneh <hugo.wendjaneh@gmail.com>
2022-06-10 10:43:49 +0100

    Elever les espace javas methode 1 et 2

--- SAE 2.02 Java/src/main/Main.java
+++ SAE 2.02 Java/src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import main.eraser.Eraser;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println(Eraser.methodeUn("C o u cou  J M  B "));
+		System.out.println(Eraser.methodeDeux("Cou cou  J M  B"));
 	}
 
 }
--- SAE 2.02 Java/src/main/eraser/Eraser.java
+++ SAE 2.02 Java/src/main/eraser/Eraser.java
@@ -1,5 +1,30 @@
 package main.eraser;
 
 public class Eraser {
+	public static String methodeUn(String req) {
+		// remplace tous les espaces qui n'ont pas d'espace avant et après par du vide
+		return req.replaceAll("(?<! ) (?! )", ""); 
+	}
 
+	public static String methodeDeux(String req) {
+		String res;
+		int i;
+		char space = ' ';
+		
+		res = "";
+		for (i = 1; i < req.length() - 1; i++) {
+			if (
+					Character.compare(req.charAt(i), space) == 0 &&
+					(
+						Character.compare(req.charAt(i-1), space) != 0 &&
+						Character.compare(req.charAt(i+1), space) != 0
+					)
+				) {
+				res += "";
+			} else {
+				res += req.charAt(i);
+			}
+		}
+		return res = "";
+	}
 }
